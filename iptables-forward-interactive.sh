bb66fd8841bba974d7a53596a47b28e9ae4172d6
RATING3PRO <xiesmail2000@gmail.com>
2026-01-15 10:21:17 +0800

    Update iptables-forward-interactive.sh

--- iptables-forward-interactive.sh
+++ iptables-forward-interactive.sh
@@ -65,12 +65,7 @@ esac
 echo
 echo "[*] 解析目标地址..."
 
-TARGET_IP=$(
-  getent hosts "$TARGET_HOST" \
-  | awk '{print $1}' \
-  | grep -v ':' \
-  | head -n1
-)
+TARGET_IP=$(getent ahostsv4 "$TARGET_HOST" | awk '{print $1}' | grep -v ':' | head -n1)
 
 if [ -z "$TARGET_IP" ]; then
   echo "[!] No IPv4 address found for $TARGET_HOST"
